eef7ad7cc85d2bde9a452dc30f6305d2d8ee7b58
Francisco Javier Marques Gaona <fmg703@inlumine.ual.es>
2018-05-18 21:31:53 +0200

    Renombrando test

--- tests/ual/hmis/asignatura/ActividadTest.java
+++ tests/ual/hmis/asignatura/ActividadTest.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import org.junit.Test;
 import ual.hmis.asignatura.*;
 
-public class TestActividadJUnit {
+public class ActividadTest {
 	
 	@Test
 	public void testNotasActividad(){
